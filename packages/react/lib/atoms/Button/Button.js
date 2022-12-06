155038fffa075277c5a25d5a57c6a70f57ecdf4c
HaiTrinh1511 <67938967+HaiTrinh1511@users.noreply.github.com>
2022-12-06 17:20:09 +0700

    Name: Setup dev script for all packages, monorepo

---
+++ packages/react/lib/atoms/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+var Button = function (_a) {
+    var label = _a.label;
+    return React.createElement("button", { className: "dse-button__container" }, label);
+};
+
+export { Button as default };
+//# sourceMappingURL=Button.js.map
